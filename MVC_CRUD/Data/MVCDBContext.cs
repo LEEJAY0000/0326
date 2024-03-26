c0566524834d155e293b405b61b60db3f6188d19
LEEJAY <zzz022679@gmail.com>
2024-03-26 17:50:12 +0800

    0326 01

---
+++ MVC_CRUD/Data/MVCDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_CRUD.Data
+{
+    public class MVCDBContext : DbContext
+    {
+        public MVCDBContext(DbContextOptions<MVCDBContext> options) : base(options)
+        {
+        }
+    }
+}
